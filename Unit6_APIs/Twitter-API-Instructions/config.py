d53f81f636efd18697b290577b605d506681f258
kandrar <61708426+kandrar@users.noreply.github.com>
2020-05-22 17:09:07 -0400

    Twitter API Homework

---
+++ Unit6_APIs/Twitter-API-Instructions/config.py
@@ -0,0 +1,11 @@
+api_key= "3e0a7a435amsh242430dabe82986p1ea071jsn569dac7090fe"
+
+#Google Key
+gkey='AIzaSyCMttLELlI-SCFgb3hqhI2L_dNgM8KjwwI'
+
+# Twitter API Keys
+
+consumer_key = "8tw9qyduaRLOeESUzdOvLthAi"
+consumer_secret = "s0rCX02eKoIRSWboHdPAZekYPh8UCVhKIB9qvsWJCLOyDx1Dc8"
+access_token = "1255169296893960192-FbR5WGCjONvuZtiIN3fIpJSDsLBDY0"
+access_token_secret = "AxiPSMEY3ItH2kZIXnmUM3V2WzGDTMXLxagPUjcWKekal"
